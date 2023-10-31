9958a3d4549b05b2eeae780cdeab8355597b6103
JakubZmatlo <Zedkos@post.cz>
2023-10-31 15:19:24 +0100

    osu

---
+++ res/js/main.js
@@ -0,0 +1,68 @@
+const point = document.getElementById("point");
+const timeInfo = document.getElementById("timeInfo");
+const startGame = document.getElementById("startGame");
+
+let pointInterval;
+
+startGame.onclick = () => {
+  startGame.style.display = "none";
+  startPointInterval();
+};
+
+const movePoint = (element, x, y) => {
+  element.style.top = `${y}px`;
+  element.style.left = `${x}px`;
+};
+
+const startPointInterval = () => {
+  clearInterval(pointInterval);
+  setPointClick(point);
+  pointInterval = setInterval(() => {
+    setSize(point, getRandomNumber(40, 70));
+    movePoint(
+      point,
+      getRandomNumber(
+        parseFloat(point.style.width),
+        window.innerWidth - parseFloat(point.style.width)
+      ),
+      getRandomNumber(
+        parseFloat(point.style.height),
+        window.innerHeight - parseFloat(point.style.height)
+      )
+    );
+  }, 600);
+};
+
+const getRandomNumber = (minimum, maximum) =>
+  Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+
+let timeStart = 0;
+const setPointClick = (element) => {
+  element.onclick = () => {
+    if (timeStart == 0) {
+      timeStart = performance.now();
+    } else {
+      let timeEnd = performance.now();
+      let result = timeEnd - timeStart;
+      timeInfo.innerText = `Time: ${result}ms`
+      timeStart = performance.now();
+    }
+    element.innerText++;
+    movePoint(
+      point,
+      getRandomNumber(
+        parseFloat(point.style.width),
+        window.innerWidth - parseFloat(point.style.width)
+      ),
+      getRandomNumber(
+        parseFloat(point.style.height),
+        window.innerHeight - parseFloat(point.style.height)
+      )
+    );
+  };
+};
+
+const setSize = (element, size) => {
+  element.style.width = `${size}px`;
+  element.style.height = `${size}px`;
+};
